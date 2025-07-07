7b066287965935668b8c82a18f3cc7a8b15c7192
Esraa Ahmed <esraaabdelrazek12@gmail.com>
2025-07-07 03:42:45 +0300

    add & update item in cart

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,33 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import userRoute from './routes/userRoute';
-import productRoute from './routes/productRoute';
-import { seedInitialProducts } from './services/productService';
+import dotenv from "dotenv";
 
+import express from "express";
+import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
+import productRoute from "./routes/productRoute";
+import cartRoute from "./routes/cartRoute";
+import { seedInitialProducts } from "./services/productService";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
-const port = 3001 ; 
+const port = 3001;
 
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cors());
+
 mongoose
-    .connect('mongodb://localhost:27017/ecommerce')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('Error connecting to MongoDB:', err));;
-
+  .connect(process.env.DATABASE_URL || "")
+  .then(() => console.log("Mongo connected!"))
+  .catch((err) => console.log("Failed to connect!", err));
 
 // Seed the products to database
 seedInitialProducts();
 
-
-app.use('/user', userRoute)
+app.use("/user", userRoute);
 app.use("/product", productRoute);
+app.use("/cart", cartRoute);
+
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-}); 
+  console.log(`Server is running at: http://localhost:${port}`);
+});
